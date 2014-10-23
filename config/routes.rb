880e7712113b980179673f2f6b3f11ec78e5998f
Daniel Robinson <dan@tag.ca>
2014-10-23 10:18:13 -0400

    changed naming scheme

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'presenter#show_home_page'
+  root 'presenter#home_page'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
