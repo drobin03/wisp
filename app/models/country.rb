7c3858346fa07542be2fd8dcdacc642240cf78b9
Daniel Robinson <dan@tag.ca>
2014-10-23 09:38:40 -0400

    added validations

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,4 +1,6 @@
 class Country < ActiveRecord::Base
   has_many :provinces, dependent: :destroy
-  
+
+  validates :name, :longitude, :latitude, presence: true
+
 end
--- app/models/province.rb
+++ app/models/province.rb
@@ -1,4 +1,6 @@
 class Province < ActiveRecord::Base
   belongs_to :country
   has_many :cities, dependent: :destroy
+
+  validates :country, :name, :longitude, :latitude, presence: true
 end
