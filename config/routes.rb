bfe79964ec431996746119176fad6ea561e11739
Daniel Robinson <dan@tag.ca>
2014-10-30 11:21:10 -0400

    added route for getting rankings

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get 'rankings' => 'presenter#rankings_page'
   get 'conversations' => 'presenter#conversations_page'
 
-  
+  get 'rankings/city/list' => 'ranking#city_ranked_isp_list'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
