85bbdc414a2cac68289700a5face5967908766df
Daniel Robinson <dan@tag.ca>
2014-10-30 09:19:22 -0400

    added routes for rankings and conversations pages

--- app/controllers/presenter_controller.rb
+++ app/controllers/presenter_controller.rb
@@ -3,4 +3,11 @@ class PresenterController < ApplicationController
   def home_page
     @countries = Country.all
   end
+
+  def rankings_page
+  end
+  
+  def conversations_page
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'presenter#home_page'
+
+  get 'rankings' => 'presenter#rankings_page'
+  get 'conversations' => 'presenter#conversations_page'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
