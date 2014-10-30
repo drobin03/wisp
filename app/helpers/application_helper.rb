a0ad97a283590cee8b5fb360a9f69d1ec2ccee4c
Nick <nicholasrbruner@gmail.com>
2014-10-30 10:16:31 -0400

    active navbar links for each page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def active_path(page)
+        if(current_page?(page))
+            return "class=active"
+        else
+            return ""
+        end
+    end
 end
