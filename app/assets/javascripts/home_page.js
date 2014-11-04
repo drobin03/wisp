bed0127c3c9b7ae08c1182db96eee9331725b78b
Nick <nicholasrbruner@gmail.com>
2014-11-04 10:03:06 -0500

    added example for sending the city through ajax

--- app/assets/javascripts/home_page.js
+++ app/assets/javascripts/home_page.js
@@ -4,12 +4,25 @@ $( document ).ready(function() {
     
 });
 
+// defaults to guelph
 $.ajax({
     url: "rankings/city/list",
     dataType: "json",
     }).done(function( data ) {
         console.log( "done: " + data.message );
-        console.log( "isps: " + data.isps );
+        console.dir( data.isps );
+    }).fail(function(jqXHR, msg) {
+        alert( "error " + msg);
+  });
+  
+  // push city
+  $.ajax({
+    url: "rankings/city/list",
+    data: { city: "toronto" },
+    dataType: "json",
+    }).done(function( data ) {
+        console.log( "done: " + data.message );
+        console.dir( data.isps );
     }).fail(function(jqXHR, msg) {
         alert( "error " + msg);
   });
