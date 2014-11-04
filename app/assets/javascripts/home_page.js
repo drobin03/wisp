1166791e90dd407af61464f2df847c121d3f08b4
Daniel Robinson <dan@tag.ca>
2014-11-04 09:27:35 -0500

    returning a list of isps

--- app/assets/javascripts/home_page.js
+++ app/assets/javascripts/home_page.js
@@ -9,6 +9,7 @@ $.ajax({
     dataType: "json",
     }).done(function( data ) {
         console.log( "done: " + data.message );
+        console.log( "isps: " + data.isps );
     }).fail(function(jqXHR, msg) {
         alert( "error " + msg);
   });
--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,7 +1,11 @@
 class RankingController < ApplicationController
   respond_to :html, :js
   def city_ranked_isp_list
+    name = params["city"].nil? ? "Guelph" : params["city"]
+    city = City.find_by(name: name.downcase)
+    
     return_val = Hash.new
+    return_val[:isps] = city.isps
     return_val[:message] = "your list!"
     render json: return_val
   end
