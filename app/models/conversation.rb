a0674374163d2555b95006febe7d25829385ac94
barrickjake <barrick.jake@gmail.com>
2014-11-06 10:09:17 -0500

    Fixed YEE

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,7 +1,6 @@
 class Conversation < ActiveRecord::Base
   belongs_to :isp
 
-<<<<<<< HEAD
   validates :user_name, :isp, :body, presence: true
 
   def date
@@ -12,8 +11,5 @@ class Conversation < ActiveRecord::Base
   	#self.province + "/" + self.city + "/" self.isp
   	self.isp
   end
-=======
   validates :user_name, :isp, :subject, :body, presence: true
-  validates :city, presence: true, if => "!province.empty?"
->>>>>>> 8196524895cef3f6a137d2303eb7a72d21dbe56b
 end
