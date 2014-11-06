d2d4933780905453e31a272884336322e54b6e20
Yee Hong Chu <ychu@mail.uoguelph.ca>
2014-11-06 09:42:23 -0500

    add more content to conversation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141106135842) do
+ActiveRecord::Schema.define(version: 20141106142007) do
 
   create_table "cities", force: true do |t|
     t.integer  "province_id"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20141106135842) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "province_id"
   end
 
   add_index "conversations", ["isp_id"], name: "index_conversations_on_isp_id", using: :btree
