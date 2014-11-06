62e8d9f8e40e74216d5b6b9dba7c6196b1d0885d
barrickjake <barrick.jake@gmail.com>
2014-11-06 10:03:56 -0500

    Conversations page pulls from db

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -2,4 +2,13 @@ class Conversation < ActiveRecord::Base
   belongs_to :isp
 
   validates :user_name, :isp, :body, presence: true
+
+  def date
+  	self.created_at.strftime("%d/%m/%Y %I:%M%p")
+  end
+
+  def breadcrumbs
+  	#self.province + "/" + self.city + "/" self.isp
+  	self.isp
+  end
 end
