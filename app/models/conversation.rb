8196524895cef3f6a137d2303eb7a72d21dbe56b
Yee Hong Chu <ychu@mail.uoguelph.ca>
2014-11-06 09:59:20 -0500

    add another new column to conversations

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,5 +1,6 @@
 class Conversation < ActiveRecord::Base
   belongs_to :isp
 
-  validates :user_name, :isp, :body, presence: true
+  validates :user_name, :isp, :subject, :body, presence: true
+  validates :city, presence: true, if => "!province.empty?"
 end
