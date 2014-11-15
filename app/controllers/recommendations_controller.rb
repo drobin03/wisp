d15769291680f5fe6ed07820571bfabd1a96bac2
Daniel Robinson <d.robinson004@gmail.com>
2014-11-15 18:43:57 -0500

    recommendations form actually changes results now

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -7,10 +7,23 @@ class RecommendationsController < ApplicationController
     isps = city.isps
 
     # Select the isp with the highest download/cost
-    # Filter results by params
-    valid_isps = isps.where('cable_price > 0')
     # sort by (max download speed measured / cable price)
-    @isp = valid_isps.sort_by{ |isp| isp.avg_download_kbps.nil? ? -1 : isp.avg_download_kbps/isp.cable_price }.last
+    isps = isps.select("isps.*, MIN(isps.cable_price) AS cable_price, AVG(speed_test_results.download_kbps) AS avg_download, AVG(speed_test_results.upload_kbps) AS avg_upload, AVG(speed_test_results.download_kbps)/MIN(isps.cable_price) AS download_per_dollar").
+                joins("JOIN speed_test_results ON isps.isp_company_id = speed_test_results.isp_company_id AND isps.city_id = speed_test_results.city_id").
+                where("isps.cable_price > 0").
+                group(:isp_company_id).
+                order("download_per_dollar DESC").
+                joins(:isp_company)
+
+    min_download = params[:recommendation][:download_mbps]
+    min_upload = params[:recommendation][:upload_mbps]
+    max_cost = params[:recommendation][:cost]
+    
+    isps = isps.find_all{ |isp| isp.avg_download / 1000 > min_download.to_f } unless min_download.empty?
+    isps = isps.find_all{ |isp| isp.avg_upload / 1000 > min_upload.to_f } unless min_upload.empty?
+    isps = isps.find_all{ |isp| isp.cable_price <= max_cost.to_f } unless max_cost.empty?
+    @isp = isps.first
+    # valid_isps.sort_by{ |isp| isp.avg_download_kbps.nil? ? -1 : isp.avg_download_kbps/isp.cable_price }.last
   end
 
 end
