27a653d483242742a79dc977ea9fdbbad4ab5dc3
Daniel Robinson <d.robinson004@gmail.com>
2014-11-17 14:53:57 -0500

    limited results to 10 isps, #6

--- app/controllers/city_controller.rb
+++ app/controllers/city_controller.rb
@@ -17,9 +17,17 @@ class CityController < ApplicationController
   def isp_list
     city = City.find(params[:id])
     oldestDate = Time.now - 1.year
+
+    top_isps = city.speed_test_results.select("isp_company_id, AVG(download_kbps) as avg_download").
+      where("date > \"#{oldestDate}\"").
+      group("isp_company_id").
+      order("avg_download DESC").
+      limit(10).
+      map(&:isp_company_id)
+    
     isps = city.speed_test_results.select("isp_company_id, isp_companies.name, date, AVG(download_kbps) as avg_download").
       joins(:isp_company).
-      where("date > \"#{oldestDate}\"").
+      where("date > \"#{oldestDate}\"").where(isp_company_id: top_isps).
       group("isp_company_id, YEAR(date), MONTH(date)").to_json
     render json: isps
   end
