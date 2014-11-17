d8a39a4cf8dbc8d2ac1d7f6d07e669542965d103
Daniel Robinson <d.robinson004@gmail.com>
2014-11-17 16:40:51 -0500

    added deployment resources

---
+++ Capfile
@@ -0,0 +1,13 @@
+# Load DSL and Setup Up Stages
+require 'capistrano/setup'
+
+# Includes default deployment tasks
+require 'capistrano/deploy'
+
+require 'capistrano/bundler'
+require 'capistrano/rbenv'
+require 'capistrano/rails'
+require 'capistrano/rails/assets'
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.1.3'
---
+++ config/deploy.rb
@@ -0,0 +1,52 @@
+# config valid only for Capistrano 3.1
+lock '3.1.0'
+
+set :application, 'iamdan'
+set :repo_url, 'git@github.com:drobin03/wisp.git'
+
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+# Default deploy_to directory is /var/www/my_app
+set :deploy_to, '/home/dan/wisp'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+set :linked_files, %w{config/database.yml .rbenv-vars}
+
+# Default value for linked_dirs is []
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+set :secret_key_base, {
+  'SECRET_KEY_BASE' => "rake secret"
+}
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
+end
